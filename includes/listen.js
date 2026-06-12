810dd00acbd2ac00d462808692ba1c90879402be
NYAN <vuinguyen16003069@gmail.com>
2026-06-12 15:55:14 +0700

    feat: implement listener module to handle bot events, data loading, and thread approval validation

--- includes/listen.js
+++ includes/listen.js
@@ -18,60 +18,60 @@ module.exports = ({ api, models }) => {
     `${api.getCurrentUserID()} - [ ${global.config.PREFIX} ] • ${!global.config.BOTNAME ? 'This bot was made by CatalizCS and SpermLord' : global.config.BOTNAME}`,
     '[ BOT INFO ] >'
   )
-    ; (async () => {
-      try {
-        logger.loader('Tiến hành tải dữ liệu người dùng và nhóm')
-        const [threads, users, currencies] = await Promise.all([
-          Threads.getAll(),
-          Users.getAll(['userID', 'name', 'data']),
-          Currencies.getAll(['userID']),
-        ])
-        for (let i = 0; i < threads.length; i++) {
-          const data = threads[i]
-          const idThread = String(data.threadID)
-          global.data.allThreadID.push(idThread)
-          global.data.threadData.set(idThread, data.data || {})
-          global.data.threadInfo.set(idThread, data.threadInfo || {})
-          if (data.data?.banned) {
-            global.data.threadBanned.set(idThread, {
-              reason: data.data.reason || '',
-              dateAdded: data.data.dateAdded || '',
-            })
-          }
-          if (data.data?.commandBanned?.length) {
-            global.data.commandBanned.set(idThread, data.data.commandBanned)
-          }
-          if (data.data?.NSFW) {
-            global.data.threadAllowNSFW.push(idThread)
-          }
+  ;(async () => {
+    try {
+      logger.loader('Tiến hành tải dữ liệu người dùng và nhóm')
+      const [threads, users, currencies] = await Promise.all([
+        Threads.getAll(),
+        Users.getAll(['userID', 'name', 'data']),
+        Currencies.getAll(['userID']),
+      ])
+      for (let i = 0; i < threads.length; i++) {
+        const data = threads[i]
+        const idThread = String(data.threadID)
+        global.data.allThreadID.push(idThread)
+        global.data.threadData.set(idThread, data.data || {})
+        global.data.threadInfo.set(idThread, data.threadInfo || {})
+        if (data.data?.banned) {
+          global.data.threadBanned.set(idThread, {
+            reason: data.data.reason || '',
+            dateAdded: data.data.dateAdded || '',
+          })
         }
-        for (let i = 0; i < users.length; i++) {
-          const dataU = users[i]
-          const idUsers = String(dataU.userID)
-          global.data.allUserID.push(idUsers)
-          if (dataU.name?.length) {
-            global.data.userName.set(idUsers, dataU.name)
-          }
-          if (dataU.data?.banned) {
-            global.data.userBanned.set(idUsers, {
-              reason: dataU.data.reason || '',
-              dateAdded: dataU.data.dateAdded || '',
-            })
-          }
-          if (dataU.data?.commandBanned?.length) {
-            global.data.commandBanned.set(idUsers, dataU.data.commandBanned)
-          }
+        if (data.data?.commandBanned?.length) {
+          global.data.commandBanned.set(idThread, data.data.commandBanned)
         }
-        for (let i = 0; i < currencies.length; i++) {
-          const dataC = currencies[i]
-          global.data.allCurrenciesID.push(String(dataC.userID))
+        if (data.data?.NSFW) {
+          global.data.threadAllowNSFW.push(idThread)
         }
-        logger.loader(`Tải thành công dữ liệu của ${global.data.allThreadID.length} nhóm`)
-        logger.loader(`Tải thành công dữ liệu của ${global.data.allUserID.length} người dùng`)
-      } catch (error) {
-        logger(`Tải môi trường thất bại: ${error}`, 'error')
       }
-    })()
+      for (let i = 0; i < users.length; i++) {
+        const dataU = users[i]
+        const idUsers = String(dataU.userID)
+        global.data.allUserID.push(idUsers)
+        if (dataU.name?.length) {
+          global.data.userName.set(idUsers, dataU.name)
+        }
+        if (dataU.data?.banned) {
+          global.data.userBanned.set(idUsers, {
+            reason: dataU.data.reason || '',
+            dateAdded: dataU.data.dateAdded || '',
+          })
+        }
+        if (dataU.data?.commandBanned?.length) {
+          global.data.commandBanned.set(idUsers, dataU.data.commandBanned)
+        }
+      }
+      for (let i = 0; i < currencies.length; i++) {
+        const dataC = currencies[i]
+        global.data.allCurrenciesID.push(String(dataC.userID))
+      }
+      logger.loader(`Tải thành công dữ liệu của ${global.data.allThreadID.length} nhóm`)
+      logger.loader(`Tải thành công dữ liệu của ${global.data.allUserID.length} người dùng`)
+    } catch (error) {
+      logger(`Tải môi trường thất bại: ${error}`, 'error')
+    }
+  })()
   const handlers = {}
   fs.readdirSync(path.join(__dirname, './handle')).forEach((file) => {
     handlers[path.basename(file, '.js')] = require(`./handle/${file}`)({
